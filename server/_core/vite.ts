412306319c8039bec21a7b89cc89ed8fd8577d72
almanalaysay93-gif <almanalaysay93-gif@users.noreply.github.com>
2026-08-16 13:42:36 +0800

    Optimize page load speed with asset preloading and static cache headers

--- server/_core/vite.ts
+++ server/_core/vite.ts
@@ -58,7 +58,7 @@ export function serveStatic(app: Express) {
     );
   }
 
-  app.use(express.static(distPath));
+  app.use(express.static(distPath, { maxAge: "1d", etag: true }));
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
