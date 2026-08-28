8f7ac6d95c8b2daf1d49cd7a37a505bca9d1deff
almanalaysay <almanalaysay93@gmail.com>
2026-08-28 11:16:32 +0800

    Bundle Vercel API dependencies

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,14 @@
 import "dotenv/config";
-import type { Request, Response } from "express";
+import express, { type Request, type Response } from "express";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { registerOAuthRoutes } from "../server/_core/oauth";
+import { registerStorageProxy } from "../server/_core/storageProxy";
+import { appRouter } from "../server/routers";
+import { createContext } from "../server/_core/context";
 
 let appPromise: Promise<any> | null = null;
 
 async function getApp() {
-  const express = (await import("express")).default;
-  const { createExpressMiddleware } = await import("@trpc/server/adapters/express");
-  const { registerOAuthRoutes } = await import("../server/_core/oauth");
-  const { registerStorageProxy } = await import("../server/_core/storageProxy");
-  const { appRouter } = await import("../server/routers");
-  const { createContext } = await import("../server/_core/context");
-
   const app = express();
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
